fe44c01a247b252847c68719a2033db9c87066bc
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-06-23 10:55:08 -0400

    adding components

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,5 @@
-<script setup lang="ts">
-const runtimeConfig = useRuntimeConfig();
-const colors = [
-  "#f87171",
-  "#fb923c",
-  "#fbbf24",
-  "#facc15",
-  "#a3e635",
-  "#4ade80",
-  "#34d399",
-  "#2dd4bf",
-  "#22d3ee",
-  "#38bdf8",
-  "#60a5fa",
-  "#818cf8",
-  "#a78bfa",
-  "#c084fc",
-  "#e879f9",
-  "#f472b6",
-  "#fb7185",
-];
-const color = useState(
-  "color",
-  () => colors[Math.floor(Math.random() * colors.length)]
-);
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div
-    class="absolute inset-0 flex flex-col items-center justify-center w-full text-center font-sans"
-  >
-    <h1 :style="{ color }" class="text-4xl md:text-7xl font-bold mb-8">
-      {{ runtimeConfig.public.helloText }}
-    </h1>
-    <NuxtLink to="/" external class="text-gray-500 text-lg hover:underline">
-      refresh
-    </NuxtLink>
-  </div>
+  <Hero />
 </template>
