b1966f1ab5c11c93bd50bd444d588b79603a6c39
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-06-25 11:45:28 -0400

    adding real leads count

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,16 @@
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://datafa.st/js/script.js",
+      defer: true,
+      "data-website-id": "685c194a287f15162381fa76",
+      "data-domain": "live.spygrocery.com",
+    },
+  ],
+});
+</script>
+
 <template>
   <Header />
   <NuxtPage />
