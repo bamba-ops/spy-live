4f8978aa9878df20f531c23eb743979e33d4d153
Walid Gharbi <51869591+bamba-ops@users.noreply.github.com>
2025-07-04 17:32:47 -0400

    Improve SEO meta for index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'SpyLive : Vendre sur TikTok Live et encaisser en 1 clic',
+  description:
+    'Augmentez vos revenus sur TikTok Live grâce à SpyLive. Génération automatique de liens Stripe pour un paiement instantané.',
+  ogTitle: 'SpyLive : Vendre sur TikTok Live et encaisser en 1 clic',
+  ogDescription:
+    'Avec SpyLive, transformez chaque commentaire en vente sans panier et encaissez directement via Stripe.',
+  ogImage:
+    'https://bywesphmqjoadfyrxbyu.supabase.co/storage/v1/object/public/test//spylive_logo.png',
+  ogType: 'website',
+  twitterCard: 'summary_large_image'
+})
+
+useHead({
+  meta: [
+    {
+      name: 'keywords',
+      content:
+        'spylive, vendre sur tiktok live, gagner argent tiktok, lien paiement automatique, stripe, tiktok live shopping'
+    }
+  ]
+})
+</script>
 
 <template>
   <div>
